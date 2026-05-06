23e8d95d437fe98a32a7e9f77fdae95ea02a5558
humboldt-dev-2025 <santiagogmm1101@gmail.com>
2026-05-06 12:08:48 -0500

    Update app.jsx

--- app.jsx
+++ app.jsx
@@ -27,7 +27,7 @@ const App = () => {
 
   return (
     <div style={styles.container}>
-      <h1 style={styles.title}>Mi sitio con Re</h1>
+      <h1 style={styles.title}>Mi sitio con santiago</h1>
       <p style={styles.subtitle}>Este es un proyecto simple listo para publicar.</p>
 
       <div style={styles.card}>
